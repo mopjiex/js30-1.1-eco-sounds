91b5bd0653f72cc39bcc8c74625bb4a89f36d0c0
mopjiex <artem1bannikov@gmail.com>
2022-02-07 19:17:35 +0300

    feat: добавил смену картинок при нажатии кнопок и проигрывание музыки

--- js/index.js
+++ js/index.js
@@ -1,47 +1,13 @@
-function removeAddClass(menu) {
-    for(let i of menu) {
-        i.addEventListener('click', () => {
-            if(!(i.classList.contains('active'))) {
-                for(let i of menu) {
-                    i.classList.remove('active');
-                }
-                i.classList.add('active');
 
-            }
-        });
-        
-    }
-}
-
-function backgroundChange(menu) {
-    for(let i of menu) {
-        removeAddClass(menu);
-        i.addEventListener('click', () => {
-            if(i.classList.contains('solovei')) {
-                main.style.background = `url(/images/solovey.jpg)`;
-            } 
-            else if(i.classList.contains('drozd')) {
-                main.style.background = `url(/images/drozd.jpg)`;
-            }
-            else if(i.classList.contains('malinovka')) {
-                main.style.background = `url(/images/zarynka.jpg)`;
-            }
-            else if(i.classList.contains('javoronok')) {
-                main.style.background = `url(/images/javoronok.jpg)`;
-            }
-            else if(i.classList.contains('slavka')) {
-                main.style.background = `url(/images/slavka.jpg)`;
-            }
-        });
-        
-    }
-}
-
-
-let isplay = false;
 const audio = new Audio();
-function playAudio() {
-    audio.src = '/sound/solovey.mp3';
+const main = document.querySelector('.main');
+const headerMenuLink = document.querySelectorAll('.header__menu-link');
+const mainLinkAudio = document.querySelector('.main__link-audio');
+const mainAudioImg = document.querySelector('.main__audio-img');
+let src = '';
+
+function playAudio(src) {
+    audio.src = src;
     audio.currentTime = 0;
     audio.play();
 }
@@ -51,57 +17,51 @@ function pauseAudio() {
 }
 
 
+for(let i = 0; i < headerMenuLink.length; i++) {
+    headerMenuLink[i].addEventListener('click', function()  {
+        isPlay = true;
+        for(let i = 0; i < headerMenuLink.length; i++) {
+            headerMenuLink[i].classList.remove('active');
+        }
+        this.classList.add('active');
 
+        if(headerMenuLink[i].classList.contains('solovei')) {
+            main.style.background = `url(/images/solovey.jpg)`;
+            src = './sound/solovey.mp3';
 
+        } 
+        else if(headerMenuLink[i].classList.contains('drozd')) {
+            main.style.background = `url(/images/drozd.jpg)`;
+            src = './sound/drozd.mp3';
+        }
+        else if(headerMenuLink[i].classList.contains('malinovka')) {
+            main.style.background = `url(/images/zarynka.jpg)`;
+            src = './sound/zarynka.mp3';
+        }
+        else if(headerMenuLink[i].classList.contains('javoronok')) {
+            main.style.background = `url(/images/javoronok.jpg)`;
+            src = './sound/javoronok.mp3';
+        }
+        else if(headerMenuLink[i].classList.contains('slavka')) {
+            main.style.background = `url(/images/slavka.jpg)`;
+            src = './sound/slavka.mp3';
+        }
+    });
 
+    mainLinkAudio.addEventListener('click', () => {
 
-const headerMenuLink = document.querySelectorAll('.header__menu-link');
-const main = document.querySelector('.main');
-const mainLinkAudio = document.querySelector('.main__link-audio');
-const mainAudioImg = document.querySelector('.main__audio-img');
-console.log(mainLinkAudio);
-
-
-
-for(let i of headerMenuLink) {
-    if(i.classList.contains('solovei active')) {
-        console.log('solovei');
-    }
-    else if(i.classList.contains('drozd active')) {
-        console.log('drozd');
-    }
-    else if(i.classList.contains('malinovka active')) {
-        console.log('malinovka');
-    }
-    else if(i.classList.contains('javoronok active')) {
-        console.log('javoronok');
-    }
-    else if(i.classList.contains('slavka active')) {
-        console.log('slavka');
-    }
+        
+        mainLinkAudio.classList.toggle('pause');
+        if(mainLinkAudio.classList.contains('pause')) {
+            mainAudioImg.src = './images/svg/play.svg';
+            pauseAudio();
+        }
+        else {
+            mainAudioImg.src = './images/svg/pause.svg';
+            playAudio(src);
+        }
+        
+    })
 }
-
-
-
-
-
-mainLinkAudio.addEventListener('click', () => {
-
-    mainLinkAudio.classList.toggle('pause');
-    console.log(mainLinkAudio);
-    if(mainLinkAudio.classList.contains('pause')) {
-        console.log(1);
-        mainAudioImg.src = './images/svg/play.svg';
-        pauseAudio();
-    }
-    else {
-        console.log(2);
-        mainAudioImg.src = './images/svg/pause.svg';
-        playAudio();
-    }
-})
-backgroundChange(headerMenuLink);
-
-
 
 
